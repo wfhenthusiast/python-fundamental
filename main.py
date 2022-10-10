0cbeef47e42feb966156a1baf127e84e1be4f8f7
Daniel Ari Hutapea <hutapeadaniel36@gmail.com>
2022-10-10 23:34:40 +0700

    ubah main

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-Python Fundamental
+"""
+Semua sintaksis dasar bahasa pemrograman terdiri dari:
+1. Sekuensial: langkah berurutan
+2. Percabangan: langkah melompat jika kondisi terpenuhi
+3. Perulangan: mengulang langkah yang sama berkali - kali selama/sampai kondisi terpenuhi
+"""
+print('hello, "world"')
+
+
+
+
